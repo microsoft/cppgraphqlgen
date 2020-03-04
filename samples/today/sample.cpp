dcbe2534783ab1aacf6858378df6d803dbb203cd
Bill Avery <billav@microsoft.com>
2020-03-04 00:08:40 -0800

    Update sample program to perform validation

--- samples/today/sample.cpp
+++ samples/today/sample.cpp
@@ -82,7 +82,7 @@ int main(int argc, char** argv)
 
 		std::cout << "Executing query..." << std::endl;
 
-		std::cout << response::toJSON(service->resolve(nullptr, *query.root, ((argc > 2) ? argv[2] : ""), response::Value(response::Type::Map)).get()) << std::endl;
+		std::cout << response::toJSON(service->resolve(nullptr, query, ((argc > 2) ? argv[2] : ""), response::Value(response::Type::Map)).get()) << std::endl;
 	}
 	catch (const std::runtime_error& ex)
 	{
