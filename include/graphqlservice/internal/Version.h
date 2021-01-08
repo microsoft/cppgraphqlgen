2275bbd9ddddff9378e310664a69107e6184d1dc
Bill Avery <wravery@gmail.com>
2021-01-08 13:28:12 -0800

    Add all generated files to the repo and fix dependencies

---
+++ include/graphqlservice/internal/Version.h
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#include <string_view>
+
+namespace graphql::internal {
+
+constexpr std::string_view FullVersion { "3.4.1" };
+
+constexpr size_t MajorVersion = 3;
+constexpr size_t MinorVersion = 4;
+constexpr size_t PatchVersion = 1;
+
+} // namespace graphql::internal
+
+#endif // VERSION_H
