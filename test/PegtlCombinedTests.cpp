bd7bcbfeea905db55c3c2e8ff757cdfff76fb3af
Bill Avery <wravery@gmail.com>
2021-05-27 11:59:23 -0700

    Split PegtlTests.cpp into 3 grammar variants to speeed up build

---
+++ test/PegtlCombinedTests.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <gtest/gtest.h>
+
+#include "graphqlservice/GraphQLGrammar.h"
+
+#include <tao/pegtl/contrib/analyze.hpp>
+
+using namespace graphql;
+using namespace graphql::peg;
+
+using namespace tao::graphqlpeg;
+
+TEST(PegtlCombinedCase, AnalyzeMixedGrammar)
+{
+	ASSERT_EQ(0, analyze<mixed_document>(true))
+		<< "there shouldn't be any infinite loops in the PEG version of the grammar";
+}
