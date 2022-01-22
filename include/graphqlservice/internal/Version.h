912daa2abd8e7173ea637517cc29c67943dc22f2
Bill Avery <billav@microsoft.com>
2022-01-21 21:33:39 -0800

    Regenerate version files

--- include/graphqlservice/internal/Version.h
+++ include/graphqlservice/internal/Version.h
@@ -10,11 +10,11 @@
 
 namespace graphql::internal {
 
-constexpr std::string_view FullVersion { "4.1.0" };
+constexpr std::string_view FullVersion { "4.1.1" };
 
 constexpr size_t MajorVersion = 4;
 constexpr size_t MinorVersion = 1;
-constexpr size_t PatchVersion = 0;
+constexpr size_t PatchVersion = 1;
 
 } // namespace graphql::internal
 
